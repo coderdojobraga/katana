ae0364eedbd13f4f43cf92b828c0d6b41c424385
Rui Lopes <ruilopesm19@gmail.com>
2025-08-01 19:57:13 +0100

    fix: improve panel left icon size on sidebar trigger

--- assets/vue/components/ui/sidebar/SidebarTrigger.vue
+++ assets/vue/components/ui/sidebar/SidebarTrigger.vue
@@ -21,7 +21,7 @@ const { toggleSidebar } = useSidebar()
     :class="cn('h-7 w-7', props.class)"
     @click="toggleSidebar"
   >
-    <PanelLeft />
+    <PanelLeft class="h-4 w-4" />
     <span class="sr-only">Toggle Sidebar</span>
   </Button>
 </template>
