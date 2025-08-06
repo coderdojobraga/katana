c33594f6c09a0c685b8ca27868005cb01ee01397
Rui Lopes <ruilopesm19@gmail.com>
2025-08-06 23:26:43 +0100

    feat: user login form (ui + functionality)

--- assets/vue/components/log-in/LoginForm.vue
+++ assets/vue/components/log-in/LoginForm.vue
@@ -1,27 +1,52 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from "vue";
-import { cn } from "@/lib/utils";
+import { Ref, ref } from "vue";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useLiveVue } from "live_vue";
 
-const props = defineProps<{
-  class?: HTMLAttributes["class"];
-}>();
+const live = useLiveVue();
+
+const form: Ref<{ email: string; password: string }> = ref({
+  email: "",
+  password: "",
+});
+
+const isSubmitting = ref(false);
+
+const submit = () => {
+  isSubmitting.value = true;
+
+  live.pushEvent(
+    "submit",
+    {
+      user: { email: form.value.email, password: form.value.password },
+    },
+    (_reply: any, _ref: Ref) => {
+      isSubmitting.value = false;
+    }
+  );
+};
 </script>
 
 <template>
-  <form :class="cn('flex flex-col gap-6', props.class)">
+  <form @submit.prevent="submit" class="flex flex-col gap-6">
     <div class="flex flex-col items-center gap-2 text-center">
       <h1 class="text-2xl font-bold">Log in to your account</h1>
-      <p class="text-muted-foreground text-sm text-balance">
-        Enter your email below to log in to your account
+      <p class="text-muted-foreground text-sm">
+        Enter your credentials below to log into your account
       </p>
     </div>
     <div class="grid gap-6">
       <div class="grid gap-3">
         <Label for="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" required />
+        <Input
+          id="email"
+          type="email"
+          placeholder="mail@coderdojobraga.org"
+          v-model="form.email"
+          required
+        />
       </div>
       <div class="grid gap-3">
         <div class="flex items-center">
@@ -33,9 +58,16 @@ const props = defineProps<{
             Forgot your password?
           </a>
         </div>
-        <Input id="password" type="password" required />
+        <Input id="password" type="password" v-model="form.password" required />
       </div>
-      <Button type="submit" class-name="w-full"> Log in </Button>
+      <Button
+        :disabled="!form.email || !form.password || isSubmitting"
+        type="submit"
+        class="w-full"
+      >
+        <span v-if="isSubmitting">Logging in...</span>
+        <span v-else>Log in</span>
+      </Button>
       <div
         class="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
       >
@@ -43,7 +75,7 @@ const props = defineProps<{
           Or continue with
         </span>
       </div>
-      <Button variant="outline" class-name="w-full">
+      <Button variant="outline" class="w-full">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path
             d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
--- lib/katana_web/live/auth/user_login_live.ex
+++ lib/katana_web/live/auth/user_login_live.ex
@@ -1,6 +1,9 @@
 defmodule KatanaWeb.UserLoginLive do
   use KatanaWeb, {:live_view, :root}
 
+  alias Katana.Accounts
+  alias KatanaWeb.UserAuth
+
   def render(assigns) do
     ~H"""
     <.vue v-component="UserLogin" />
@@ -8,8 +11,29 @@ defmodule KatanaWeb.UserLoginLive do
   end
 
   def mount(_params, _session, socket) do
-    email = Phoenix.Flash.get(socket.assigns.flash, :email)
-    form = to_form(%{"email" => email}, as: "user")
+    form = to_form(%{}, as: "user")
     {:ok, assign(socket, form: form), temporary_assigns: [form: form]}
+  end
+
+  def handle_event("submit", %{"user" => params}, socket) do
+    %{"email" => email, "password" => password} = params
+
+    if user = Accounts.get_user_by_email_and_password(email, password) do
+      {:noreply,
+       socket
+       |> put_flash(:info, "Welcome back!")
+       |> UserAuth.log_in_user(user, params)}
+    else
+      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
+      {:noreply,
+       socket
+       |> put_flash(:error, "Invalid email or password. Please try again shortly.")
+       |> reload()}
+    end
+  end
+
+  defp reload(socket) do
+    form = to_form(%{}, as: "user")
+    assign(socket, form: form)
   end
 end
