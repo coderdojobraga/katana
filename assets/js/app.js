32f11dbbdb3ef37a8db6625531b31c820bf3e55f
GuilhermePSF <guilhermepsf23@gmail.com>
2025-08-16 23:58:50 +0100

    feat: include backpex hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,29 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
+import 'phoenix_html';
+import '../css/app.css';
 
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html";
-// Establish Phoenix Socket and LiveView configuration.
-import { Socket } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
-import topbar from "topbar";
-import { getHooks } from "live_vue";
-import liveVueApp from "../vue";
-
-import "../css/app.css";
+import { Hooks as BackpexHooks } from './../../deps/backpex/assets/js/backpex.js';
+import { LiveSocket } from 'phoenix_live_view';
+import { Socket } from 'phoenix';
+import { getHooks } from 'live_vue';
+import liveVueApp from '../vue';
+import topbar from 'topbar';
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {
+  .getAttribute('content');
+
+const VueHooks = getHooks(liveVueApp);
+
+const liveSocket = new LiveSocket('/live', Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: getHooks(liveVueApp),
+  hooks: { ...VueHooks, ...BackpexHooks },
 });
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
-window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' });
+window.addEventListener('phx:page-loading-start', (_info) => topbar.show(300));
+window.addEventListener('phx:page-loading-stop', (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
