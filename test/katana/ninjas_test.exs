09dac51b8b9ca3c21e62bd97a8a8e3dddd044fd4
GuilhermePSF <guilhermepsf23@gmail.com>
2025-08-21 02:34:41 +0100

    chore: format

--- test/katana/ninjas_test.exs
+++ test/katana/ninjas_test.exs
@@ -8,7 +8,19 @@ defmodule Katana.NinjasTest do
 
     import Katana.NinjasFixtures
 
-    @invalid_attrs %{full_name: nil, birth_date: nil, has_attended_before: nil, number_of_sessions: nil, coderdojo_experience_description: nil, programming_experience_description: nil, has_medical_condition: nil, medical_condition_details: nil, additional_info: nil, image_consent: nil, belt: nil}
+    @invalid_attrs %{
+      full_name: nil,
+      birth_date: nil,
+      has_attended_before: nil,
+      number_of_sessions: nil,
+      coderdojo_experience_description: nil,
+      programming_experience_description: nil,
+      has_medical_condition: nil,
+      medical_condition_details: nil,
+      additional_info: nil,
+      image_consent: nil,
+      belt: nil
+    }
 
     test "list_ninjas/0 returns all ninjas" do
       ninja = ninja_fixture()
@@ -21,7 +33,19 @@ defmodule Katana.NinjasTest do
     end
 
     test "create_ninja/1 with valid data creates a ninja" do
-      valid_attrs = %{full_name: "some full_name", birth_date: ~D[2025-08-19], has_attended_before: true, number_of_sessions: 42, coderdojo_experience_description: "some coderdojo_experience_description", programming_experience_description: "some programming_experience_description", has_medical_condition: true, medical_condition_details: "some medical_condition_details", additional_info: "some additional_info", image_consent: true, belt: "some belt"}
+      valid_attrs = %{
+        full_name: "some full_name",
+        birth_date: ~D[2025-08-19],
+        has_attended_before: true,
+        number_of_sessions: 42,
+        coderdojo_experience_description: "some coderdojo_experience_description",
+        programming_experience_description: "some programming_experience_description",
+        has_medical_condition: true,
+        medical_condition_details: "some medical_condition_details",
+        additional_info: "some additional_info",
+        image_consent: true,
+        belt: "some belt"
+      }
 
       assert {:ok, %Ninja{} = ninja} = Ninjas.create_ninja(valid_attrs)
       assert ninja.full_name == "some full_name"
@@ -43,15 +67,33 @@ defmodule Katana.NinjasTest do
 
     test "update_ninja/2 with valid data updates the ninja" do
       ninja = ninja_fixture()
-      update_attrs = %{full_name: "some updated full_name", birth_date: ~D[2025-08-20], has_attended_before: false, number_of_sessions: 43, coderdojo_experience_description: "some updated coderdojo_experience_description", programming_experience_description: "some updated programming_experience_description", has_medical_condition: false, medical_condition_details: "some updated medical_condition_details", additional_info: "some updated additional_info", image_consent: false, belt: "some updated belt"}
+
+      update_attrs = %{
+        full_name: "some updated full_name",
+        birth_date: ~D[2025-08-20],
+        has_attended_before: false,
+        number_of_sessions: 43,
+        coderdojo_experience_description: "some updated coderdojo_experience_description",
+        programming_experience_description: "some updated programming_experience_description",
+        has_medical_condition: false,
+        medical_condition_details: "some updated medical_condition_details",
+        additional_info: "some updated additional_info",
+        image_consent: false,
+        belt: "some updated belt"
+      }
 
       assert {:ok, %Ninja{} = ninja} = Ninjas.update_ninja(ninja, update_attrs)
       assert ninja.full_name == "some updated full_name"
       assert ninja.birth_date == ~D[2025-08-20]
       assert ninja.has_attended_before == false
       assert ninja.number_of_sessions == 43
-      assert ninja.coderdojo_experience_description == "some updated coderdojo_experience_description"
-      assert ninja.programming_experience_description == "some updated programming_experience_description"
+
+      assert ninja.coderdojo_experience_description ==
+               "some updated coderdojo_experience_description"
+
+      assert ninja.programming_experience_description ==
+               "some updated programming_experience_description"
+
       assert ninja.has_medical_condition == false
       assert ninja.medical_condition_details == "some updated medical_condition_details"
       assert ninja.additional_info == "some updated additional_info"
